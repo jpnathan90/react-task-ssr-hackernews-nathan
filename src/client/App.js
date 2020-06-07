2ab6fefc0f5729c66ed40ec7de99b8d767fab723
jpnathan90 <jpnathan90@gmail.com>
2020-06-07 21:52:47 +0530

    Initial Commit

---
+++ src/client/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { renderRoutes } from 'react-router-config';
+import PropTypes from 'prop-types';
+
+import ErrorBoundary from './components/error-boundry/index';
+import Header from './components/header/index';
+import Footer from './components/footer/index';
+
+const App = ({ route }) => {
+  return (
+    <div className="container">
+      <Header />
+      <ErrorBoundary>{renderRoutes(route.routes)}</ErrorBoundary>
+      <Footer />
+    </div>
+  );
+};
+
+App.propTypes = {
+  route: PropTypes.objectOf(PropTypes.any),
+};
+
+App.defaultProps = {
+  route: null,
+};
+
+export default {
+  component: App,
+};
